e2d1fbf6483d724e5e0e87b1534b6c27626bcdeb
hiki89 <markohitl89@gmail.com>
2018-07-23 15:34:29 +0200

    Zadatak 8

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Single car</title>
-</head>
-<body>
-    <h2> Title: {{ $car->title }} </h2>
+@include('layout.master')
+    <h4> Title: {{ $car->title }} </h4>
     <p> Producer: {{ $car->producer }} </p> 
     <p> Number of doors: {{ $car->number_of_doors }} </p>
-</body>
-</html>
